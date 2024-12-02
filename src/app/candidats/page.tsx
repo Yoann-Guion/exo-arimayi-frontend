2469b768334d657e4ff4b7802f947a34fec0be71
Yoann-Guion <yoo.guion@gmail.com>
2024-12-02 17:01:39 +0100

    add back button on candidates page

--- src/app/candidats/page.tsx
+++ src/app/candidats/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import StudentForm from "../component/Studentform";
 
 export default function Page() {
@@ -5,6 +6,11 @@ export default function Page() {
     <div className="flex flex-col items-center p-6">
       <div className="w-4/6 p-8 bg-white shadow-md rounded-lg text-center">
         <h1 className="text-3xl font-bold text-black mb-6">Page candidats</h1>
+        <Link href="/">
+          <button className="absolute top-4 left-4 px-4 py-2 bg-gray-200 text-gray-800 border border-gray-400 rounded hover:bg-gray-300 focus:outline-none focus:ring">
+            Retour
+          </button>
+        </Link>
         <div className="w-full p-6 border border-gray-300 rounded-lg shadow-lg">
           <StudentForm />
         </div>
